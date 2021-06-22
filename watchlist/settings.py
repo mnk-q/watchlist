c08dd6c638eb5ed28c92c3a8d8dfd8c76b1d3312
= <=>
2021-06-22 16:48:56 +0530

    gitGuardian is good

--- watchlist/settings.py
+++ watchlist/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&9_q58)mw+3q*%!=6(f)kz*uo@s)6#ug5$_w=6jo*b^up$@lbk'
+SECRET_KEY = config('SECRET_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
